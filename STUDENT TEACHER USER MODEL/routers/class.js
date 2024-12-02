6639aef7cbe850751f1359ffe4d7c07615d7762c
vinit mittal <vinitmittal295@gmail.com>
2024-12-02 17:00:53 +0530

    added

--- STUDENT TEACHER USER MODEL/routers/class.js
+++ STUDENT TEACHER USER MODEL/routers/class.js
@@ -3,9 +3,9 @@ const router=express.Router()
 const VerifyAuth=require("../middleware/VerifyAuth")
 
 const classController=require("../controllers/class")
-router.post("/post",VerifyAuth,classController.createClass)
-router.get("/getall",VerifyAuth,classController.getAllClass)
-router.get("/:id",VerifyAuth,classController.getOneClass)
+router.post("/post",classController.createClass)
+router.get("/getall",classController.getAllClass)
+router.get("/:id",classController.getOneClass)
 router.patch("/updateone/:id",VerifyAuth,classController.updateClass)
 router.delete("/:id",VerifyAuth,classController.deleteClass)
 
